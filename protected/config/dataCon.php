3dcb16c9a9fee577d1a03c5ef3143aeb88340006
Nurul Islam <webdev.nislam@gmail.com>
2015-11-07 13:18:22 +0000

    who knows

--- protected/config/dataCon.php
+++ protected/config/dataCon.php
@@ -1,15 +1,2 @@
 <?php
 
-/*
-  'db'=>array(
-  'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-  ), */
-// uncomment the following to use a MySQL database
-$db = array(
-    'connectionString' => 'mysql:host=localhost;dbname=coz_ims_ff_db',
-    'enableParamLogging' => true,
-    'emulatePrepare' => true,
-    'username' => 'root',
-    'password' => '123456',
-    'charset' => 'utf8',
-);
