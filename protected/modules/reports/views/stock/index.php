3966799f4b7275f19eb5c1c120f21951b30f79bf
Nurul Islam <webdev.nislam@gmail.com>
2016-01-27 19:25:39 +0000

    stock report

---
+++ protected/modules/reports/views/stock/index.php
@@ -0,0 +1,28 @@
+<div class="col-lg-12">
+
+    <div class="box box-info">
+
+        <div class="box-body">
+
+            <?php
+            if ($model) {
+                $this->renderPartial('_data_table', array(
+                    'model' => $model,
+                    'msg' => $msg,
+                ));
+            } else {
+                ?>
+                <div class="center col-lg-6">
+                    <h3 class="text-aqua"><?php echo $msg; ?></h3>
+                </div>
+            <?php } ?>
+
+        </div>
+    </div>
+</div>
+
+<div class="clearfix"></div>
+
+<?php
+Yii::app()->clientScript->registerCoreScript('jquery');
+?>
