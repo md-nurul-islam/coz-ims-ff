09f0fe1033acb7b7db2c60e37a06934a55eadd02
Md. Nurul Islam <nurul.islam@venturas-bd.com>
2016-03-22 06:29:08 +0000

    removing data con from repo

--- protected/config/dataCon.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = array(
-    'connectionString' => 'mysql:host=localhost;dbname=coz_ims_ff_db',
-    'emulatePrepare' => true,
-    'username' => 'root',
-    'password' => '123456',
-    'charset' => 'utf8',
-);
