baa8ec9d2babe90fcd902fc732891f474b803f4d
Nurul Islam <webdev.nislam@gmail.com>
2016-03-22 10:58:27 +0000

    bal sal

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 $yii = dirname(__FILE__) . '/../../framworks/yii1/yii.php';
 if (strpos($_SERVER['SERVER_ADDR'], '192.168.') !== FALSE) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     $yii = dirname(__FILE__) . '/../../frameworks/yii/yii.php';
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
